00f07b03ae243eeb65f14dd2bb9ff8107e046155
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-11 22:56:09 +0200

    HomePage Route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from "react-router-dom"
+
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Switch>
+          <Route 
+            exact path="/"
+            component={HomePage} />
+        </Switch>
       </header>
     </div>
   );
