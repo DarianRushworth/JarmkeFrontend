25fb3ba13992755283c2bd90ccb91ba103b68de9
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-12 11:18:44 +0200

    Set background image

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,9 +1,36 @@
 import React from "react"
+import { Jumbotron } from "react-bootstrap"
+import "./index.css"
+
+const imageUrl = "https://res.cloudinary.com/djzjepmnr/image/upload/v1597221407/IMG_5465_gmhofp.jpg"
 
 export default function HomePage(){
+
     return(
-        <h1>
-            Welcome to Jarmke Jewellery
-        </h1>
+        <div 
+            style={
+                {
+                    backgroundImage: `url(${imageUrl})`
+                }
+            }
+            className="HomeImage">
+            <div>
+                <header>
+                    <Jumbotron>
+                        <h1>
+                            Home Page
+                        </h1>
+                    </Jumbotron>
+
+                </header>
+            </div>
+            <div>
+                <body>
+                    <p>
+                        Bio Here
+                    </p>
+                </body>
+            </div>
+        </div>
     )
 }
