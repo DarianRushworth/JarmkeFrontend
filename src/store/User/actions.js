bab0992b06e22a30881a5d284c244efb00ce0d99
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-13 15:09:28 +0200

    New user set in State

--- src/store/User/actions.js
+++ src/store/User/actions.js
@@ -1,6 +1,13 @@
 import axios from "axios"
 import { apiUrl } from "../../config/constants"
 
+function setNewUser(userData){
+    return {
+        type: "SET_NEW_USER",
+        payload: userData
+    }
+}
+
 function setUser(userData){
     return {
         type: "SET_USER",
@@ -15,7 +22,7 @@ export function getUser(email, password){
                 email, 
                 password,
             })
-            console.log("user check test:", userCheck)
+            // console.log("user check test:", userCheck)
 
             dispatch(setUser(userCheck.data))
 
@@ -44,7 +51,9 @@ export function newUser(
                     dateOfBirth,
                     password,
                 })
-                console.log("new user test", userSignedUp)
+                // console.log("new user test", userSignedUp)
+
+                dispatch(setNewUser(userSignedUp.data))
 
             } catch(error){
                 console.log(error.message)
--- src/store/User/reducer.js
+++ src/store/User/reducer.js
@@ -2,6 +2,10 @@ const initialState = {}
 
 export default function userReducer(state = initialState, action){
     switch(action.type){
+        case "SET_NEW_USER":
+        return {
+            ...action.payload
+        }
         case "SET_USER":
             return {
                 ...action.payload
