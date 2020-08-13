69499626c7d534b5989b06151227382e3e43a236
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-13 22:26:38 +0200

    Profile Page stage 2 complete, Orders inputted

--- src/components/UserOrders/index.js
+++ src/components/UserOrders/index.js
@@ -1,20 +1,30 @@
 import React from "react"
 import { Table } from "react-bootstrap"
 
+
 export default function UserOrders(props){
     const user = props.data
     console.log("user test for orders", user)
 
     const displayOrders = user.orders.map(order => {
+        const colorPicker = order.completed
+                            ? {
+                                textAlign: "center",
+                                color: "green"
+                            }
+                            : {
+                                textAlign: "center",
+                                color: "red"
+                            }
         return (
             <tr>
-                <td>
+                <td style={colorPicker}>
                     {order.total}
                 </td>
-                <td>
+                <td style={colorPicker}>
                     {JSON.stringify(order.expressShipping)}
                 </td>
-                <td>
+                <td style={colorPicker}>
                     {JSON.stringify(order.completed)}
                 </td>
             </tr>
