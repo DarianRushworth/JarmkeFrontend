50151c8b6330f7c3a1fdd584ff27ba0f8b9137a2
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-13 11:32:08 +0200

    Login Successful, user stored in State

--- src/store/User/actions.js
+++ src/store/User/actions.js
@@ -1,6 +1,13 @@
 import axios from "axios"
 import { apiUrl } from "../../config/constants"
 
+function setUser(userData){
+    return {
+        type: "SET_USER",
+        payload: userData
+    }
+}
+
 export function getUser(email, password){
     return async function thunk3(dispatch, getState){
         try{
@@ -9,6 +16,8 @@ export function getUser(email, password){
                 password,
             })
             console.log("user check test:", userCheck)
+
+            dispatch(setUser(userCheck.data))
 
         } catch(error){
             console.log(error.message)
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux"
 import ownerReducer from "./Owner/reducer"
 import productReducer from "./Products/reducer"
+import userReducer from "./User/reducer"
 
 const owner = ownerReducer
 const products = productReducer
+const user = userReducer
 
 export default combineReducers({
   owner,
   products,
+  user,
   });
