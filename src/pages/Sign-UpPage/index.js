3d74bdc9a884e2f737b3ae0c31fe710314b71fee
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-13 13:17:18 +0200

    Added type and placeholders

--- src/pages/Sign-UpPage/index.js
+++ src/pages/Sign-UpPage/index.js
@@ -9,54 +9,77 @@ import {
 export default function SignUpPage(){
     return(
         <Container>
-            <Form>
-                <h1>
+            <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+                <h1 className="mt-5 mb-5">
                     Sign Up!
                 </h1>
-                <Form.Group>
+                <Form.Group controlId="formBasicFirstName">
                     <Form.Label>
                         First Name:
                     </Form.Label>
-                    <Form.Control />
+                    <Form.Control 
+                        type="input"
+                        placeholder="First Name Here"
+                        required/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group controlId="formBasicLastName">
                     <Form.Label>
                         Last Name:
                     </Form.Label>
-                    <Form.Control />
+                    <Form.Control 
+                        type="input"
+                        placeholder="Last Name Here"
+                        required/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group controlId="formBasicEmail">
                     <Form.Label>
                         Email:
                     </Form.Label>
-                    <Form.Control />
+                    <Form.Control 
+                        type="email"
+                        placeholder= "Email Here"
+                        required/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group controlId="formBasicPhone">
                     <Form.Label>
                         Phone (with country code):
                     </Form.Label>
-                    <Form.Control />
+                    <Form.Control 
+                        type="tel"
+                        placeholder="Phone Number Here"
+                        required/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group controlId="formBasicAddress">
                     <Form.Label>
-                        Address:
+                        Postal Address:
                     </Form.Label>
-                    <Form.Control />
+                    <Form.Control 
+                        type="input"
+                        placeholder="Postal Address Here"
+                        required/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group controlId="formBasicDateOfBirth">
                     <Form.Label>
                         Date Of Birth:
                     </Form.Label>
-                    <Form.Control />
+                    <Form.Control 
+                        type="date"
+                        placeholder="DOB Here"
+                        required/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group controlId="formBasicPassword">
                     <Form.Label>
                         Password:
                     </Form.Label>
-                    <Form.Control />
+                    <Form.Control 
+                        type="password"
+                        placeholder="Password Here"
+                        required/>
                 </Form.Group>
                 <Form.Group>
-                    <Button>
+                    <Button
+                        variant="info"
+                        type="submit">
                         Sign Me Up!
                     </Button>
                 </Form.Group>
