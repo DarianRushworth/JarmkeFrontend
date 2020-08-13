2ed1846d17c07874c7579db9da4b084ee8d86fd1
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-13 11:09:25 +0200

    useState setup, with event listeners

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,12 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import {
     Form,
     Container,
-    Button
+    Button,
+    Col
 } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 export default function LoginPage(){
+    const [email, set_Email] = useState("")
+    const [password, set_Password] = useState("")
+
+    function onSubmit(event){
+        event.preventDefault()
+        console.log("Email test:", email)
+        console.log("Password test:", password)
+    }
+
     return(
         <Container>
             <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
@@ -17,7 +27,9 @@ export default function LoginPage(){
                     <Form.Label>
                         Email:
                     </Form.Label>
-                    <Form.Control 
+                    <Form.Control
+                        value={email} 
+                        onChange={(event) => set_Email(event.target.value)}
                         type="email"
                         placeholder="Enter email"
                         required/>
@@ -28,12 +40,17 @@ export default function LoginPage(){
                         Password:
                     </Form.Label>
                     <Form.Control 
+
+                        onChange={(event) => set_Password(event.target.value)}
                         type="password"
                         placeholder="Password"
                         required/>
                 </Form.Group>
                 <Form.Group className="mt-5">
-                    <Button variant="primary" type="submit">
+                    <Button 
+                        variant="primary" 
+                        type="submit"
+                        onClick={onSubmit}>
                         Log Me In
                     </Button>
                 </Form.Group>
