97c1d3478cd004329511f666ff5ddb84237934b0
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-13 17:03:28 +0200

    Product set in State

--- src/pages/ProductDetails/index.js
+++ src/pages/ProductDetails/index.js
@@ -1,6 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useParams } from "react-router"
+import { useDispatch } from "react-redux"
+
+import { getSpecificProduct } from "../../store/Products/actions"
 
 export default function ProductDetails(){
+    const dispatch = useDispatch()
+    const idNeeded = parseInt(useParams().id)
+    // console.log("params test", idNeeded)
+
+    useEffect(() => {
+        dispatch(getSpecificProduct(idNeeded))
+    })
     return (
         <h1>
             Details Page
