eb04c12babe0213c2b049715a8a6c9ddbc3a7f04
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-22 23:28:53 +0200

    Styling applied to profile page favorites

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -43,7 +43,9 @@ export default function ProfilePage(){
                 <Row>
                 <Col>
                 <Form>
-                <h1>
+                <h1 style={{
+                    fontFamily: "cursive"
+                }}>
                     User's Details:
                 </h1>
                 <Form.Group controlId="formBasicFirstName">
@@ -99,7 +101,9 @@ export default function ProfilePage(){
                 </Form>
                 </Col>
                 <Col>
-                <h1>
+                <h1 style={{
+                    fontFamily: "cursive",
+                }}>
                     Order Details:
                 </h1>
                 <UserOrders data={user}/>
