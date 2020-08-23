808edec486070211149f3dd3f134b20bce6a7f76
DarianRushworth <darianerrolrushworth@gmail.com>
2020-08-23 14:14:40 +0200

    New Footer created

--- src/components/ContactDetails/index.js
+++ src/components/ContactDetails/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react"
-import { Image, Container, Col, Row } from "react-bootstrap"
+import { Image, Container, Col, Row, Button } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
+import { useHistory } from "react-router-dom"
 import "./index.css"
 
 import { getOwnerDetails } from "../../store/Owner/actions"
 import { selectOwner } from "../../store/Owner/selectors"
 
 export default function ContactDetails(){
+    const history = useHistory()
     const dispatch = useDispatch()
     const owner = useSelector(selectOwner)
     // console.log("owner test(selector)", owner)
@@ -15,56 +17,98 @@ export default function ContactDetails(){
         
         dispatch(getOwnerDetails())
     }, [dispatch])
+
+    function redirect(){
+        history.push("/signup")
+    }
     
     return(
-            <Container className="Contact" fluid>
+        <Container>
+        <Row>
+            <Col xs={12} md={8}>
+                <h2 style={{
+                    fontFamily: "cursive",
+                    fontSize: 45,
+                }}>
+                    Contact
+                </h2>
                 <Row>
-                    <strong>Contact Details</strong>
-                </Row>
-                <Row md={6} >
-                    <Col xs={10} >
-                    <Image 
-                                src="https://res.cloudinary.com/djzjepmnr/image/upload/v1597782162/Email_ekc5jr.jpg"
-                                roundedCircle 
-                                style={{
-                                   width: 30,
-                                   height: 30,
-                                }}/>
-                    </Col>
-                    <Col>
-                        {owner.email}
+                    <Col xs={8} md={9}>
+                        <hr className="horLine"/>
                     </Col>
                 </Row>
-                <Row md={6}>
-                    <Col xs={10}>
-                    <Image 
-                                src="https://res.cloudinary.com/djzjepmnr/image/upload/v1597782043/Phone_qwzmwu.jpg"
-                                roundedCircle 
-                                style={{
-                                   width: 30,
-                                   height: 30,
-                                }}/>
-                    </Col>
-                    <Col>
-                        +{owner.phone}
-                    </Col>
-                </Row>
-                <Row md={6}>
-                    <Col xs={10}>
-                        <a href="https://www.instagram.com/_jarmke/">
-                            <Image 
-                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6EGPAjV4cLjO4uE5FjAIBvTcyWONzY-wAXw&usqp=CAU"
-                                roundedCircle 
-                                style={{
-                                   width: 30,
-                                   height: 30,
-                                }}/>
-                        </a>
-                    </Col>
-                    <Col>
-                        @_jarmke
-                    </Col>
-                </Row>
-            </Container>
+            </Col>
+        </Row>
+
+        <Row>
+            <Col>
+                <Image 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTpZ4d2bVmyOhUZ0W0YyFgyiyUDENP7IOK7UA&usqp=CAU"
+                    alt="Email Logo"
+                    rounded
+                    style={{
+                        height: 30,
+                        width: 30,
+                    }} />
+                    {owner.email}
+            </Col>
+        </Row>
+        <Row>
+            <Col style={{
+                paddingTop: 20,
+            }}>
+            <Image 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTkwOs7_zHl5ukfCD2-YwCT8WbYXyRCKgj7dw&usqp=CAU"
+                    alt="Phone Logo"
+                    rounded
+                    style={{
+                        height: 28,
+                        width: 28,
+                    }} />
+                    +{owner.phone}
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={8} md={6}>
+                <hr className="horLine"/>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={6} md={2}
+                style={{
+                    paddingTop: 10,
+                }}>
+            <Button
+                variant="outline-danger"
+                onClick={redirect}>
+                Register for Free
+            </Button>
+            </Col>
+            <Col xs={6} md={{span: 3, offset: 2}}>
+            <ul>
+        <li>
+                <Image 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWl-__Cv45Tlw4YFU3pIsRwHaKOHbiCIxn1g&usqp=CAU"
+                    alt="LinkedIn Link"
+                    rounded
+                    style={{
+                        height: 60,
+                        width: 60,
+                    }}/>                           
+        </li>
+        <li>
+                <Image 
+                    src="https://img.icons8.com/carbon-copy/2x/instagram-new.png"
+                    alt="Instagram Link"
+                    rounded
+                    style={{
+                        height: 60,
+                        width: 60,
+                    }}/>
+        </li>
+    </ul>
+            </Col>
+        </Row>
+    </Container>
     )
 }
