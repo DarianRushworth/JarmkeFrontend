4731e862dc29fbae0f5bd477d641189c565aad04
DarianRushworth <darianerrolrushworth@gmail.com>
2021-01-10 11:48:15 +0100

    Changed config of stripe setup

--- src/App.js
+++ src/App.js
@@ -12,11 +12,7 @@ import Profilepage from "./pages/ProfilePage"
 import ShoppingCart from './pages/ShoppingCart';
 import CheckoutPage from "./pages/CheckoutPage"
 import { validateUser } from "./store/User/actions"
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from "@stripe/stripe-js"
 import "./App.css"
-
-const stripePromise = loadStripe("pk_test_51HGVaXE1l9Lb6wo5PB3AAwHvhJxQvN3AHSNc9KOijpgpcB44vjw5IgF1MX09Rl5T1WFHQe7HtQS3UP0oNkNFJMiz00zLZRunTA")
 
 function App() {
   const dispatch = useDispatch()
@@ -55,7 +51,6 @@ function App() {
             path="/profilePage"
             component={Profilepage}
           />
-          <Elements stripe={stripePromise}>
           <Route
             path="/cartCheckout"
             component={ShoppingCart}
@@ -64,7 +59,6 @@ function App() {
             path="/purchase"
             component={CheckoutPage}
           />
-          </Elements>
         </Switch>
     </div>
   );
