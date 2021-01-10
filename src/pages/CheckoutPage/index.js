7ff10e1069daf079bc4b05107b54208b70001967
DarianRushworth <darianerrolrushworth@gmail.com>
2021-01-10 11:50:01 +0100

    Placed Stripe config in Page

--- src/pages/CheckoutPage/index.js
+++ src/pages/CheckoutPage/index.js
@@ -1,10 +1,16 @@
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
 import React from "react"
 import PaymentInfo from "../../components/PaymentInfo"
+
+const stripePromise = loadStripe("pk_test_51HGVaXE1l9Lb6wo5PB3AAwHvhJxQvN3AHSNc9KOijpgpcB44vjw5IgF1MX09Rl5T1WFHQe7HtQS3UP0oNkNFJMiz00zLZRunTA")
 
 export default function CheckoutPage(){
     return (
         <div>
-            <PaymentInfo />
+            <Elements stripe={stripePromise}>
+                <PaymentInfo />
+            </Elements>
         </div>
     )
 }
